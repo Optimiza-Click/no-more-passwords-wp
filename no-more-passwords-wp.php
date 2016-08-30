c8980711eb17429e1439695327f7d3d90c2d7a10
oarmesto <oarmesto@optimizaclick.com>
2016-08-30 14:55:16 +0200

    Segundo commit

--- no-more-passwords-wp.php
+++ no-more-passwords-wp.php
@@ -20,5 +20,29 @@ function nmpw_admin_menu()
 //ACCION INICIAL 
 add_action( 'admin_init', 'nmpw_admin_menu' );
 
+//SE REGISTRAN LAS ACTIVIDADES CRON DEL PLUGIN AL SER ACTIVADO
+register_activation_hook(__FILE__, 'activate_nmpw_cron');
+
+//SE ASOCIA UNA FUNCION AL ACTIVARSE EL PLUGIN
+function activate_nmpw_cron() 
+{
+	//SE REGISTRA UNA ACCION PARA QUE SE EJECUTE DIARIAMENTE
+    if (! wp_next_scheduled ( 'nmpw_auto_update' )) 
+		wp_schedule_event(time(), 'daily', 'nmpw_auto_update');
+}
+
+//SE ASOCIA LAS FUNCION QUE REALIZARA LA ACCION DE LA ACTIVIDAD DEL CRON
+add_action('nmpw_auto_update', 'nmpw_update');
+
+
+//SE ASOCIA UNA FUNCION AL DESACTIVAR EL PLUGIN
+register_deactivation_hook(__FILE__, 'desactivate_nmpw_cron');
+
+//SE CANCELA LA ACTIVIDAD CRON DEL PLUGIN AL SER DESACTIVADO
+function desactivate_nmpw_cron() 
+{
+	wp_clear_scheduled_hook('nmpw_auto_update');
+}
+
 
 ?>
